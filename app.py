8eab6c21a40e1e12a84a6dceb5db63fe0ddc4db8
naoufel gif <naoufelmajdoub90@gmail.com>
2026-05-19 21:50:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
-#python for chatbot
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+client = OpenAI(api_key=os.getenv("9sLqF6Ffx7gmbMYcJbW2eJCdtnF7SzDd"))
+print("key:",os.getenv("9sLqF6Ffx7gmbMYcJbW2eJCdtnF7SzDd"))
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    data = request.json
+    user_message = data.get("message")
+
+    response = client.chat.completions.create(
+        model="gpt-4.1-mini",
+        messages=[
+            {"role": "user", "content": user_message}
+        ]
+    )
+
+    reply = response.choices[0].message.content
+
+    return jsonify({"reply": reply})
+
+if __name__ == "__main__":
+    app.run(debug=True)
